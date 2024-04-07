8a05b7e682a4a1d85fb253be63484d5c81b6c194
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-04-07 19:49:31 +0400

    blue done

--- src/components/result/Result.jsx
+++ src/components/result/Result.jsx
@@ -6,11 +6,11 @@ export default function Result() {
       <div className="blue">
         <span className="result-span">Your Result</span>
         <div className="circle">
-          <h1>76</h1>
-          <span>of 100</span>
+          <h1 className="overall">76</h1>
+          <span className="hundred">of 100</span>
         </div>
-        <h2>Great</h2>
-        <p>
+        <h2 className="great">Great</h2>
+        <p className="perfomance">
           Your performance exceed 65% of the people conducting the test here!
         </p>
       </div>
